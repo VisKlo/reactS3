e413c553d3f6ebf72acab06750fdfb2c551d63a3
Clovis <jocailleclovis59@mgmail.com>
2025-01-27 15:48:42 +0100

    Greeting

--- src/MyButton.tsx
+++
@@ -1,8 +0,0 @@
-interface IMyButtonProps {
-    label: string;
-    onClick: () => void;
-}
-
-export const MyButton: React.FC<IMyButtonProps> = ({ label, onClick }) => {
-    return <button onClick={onClick}>{ label }</button>    
-}
